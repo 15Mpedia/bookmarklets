60e8be34643a8639af8bf4a676d30e6892852564
Emilio J. Rodríguez-Posada <emijrp@gmail.com>
2014-11-07 13:31:04 +0100

    librered

--- add-news.js
+++ add-news.js
@@ -4,7 +4,20 @@ function pad(n, l) {
  while (s.length<l) { s='0'+s; }   
  return s;
 }
- 
+var mes2num = Array();
+mes2num["enero"] = "01";
+mes2num["febrero"] = "02";
+mes2num["marzo"] = "03";
+mes2num["abril"] = "04";
+mes2num["mayo"] = "05";
+mes2num["junio"] = "06";
+mes2num["julio"] = "07";
+mes2num["agosto"] = "08";
+mes2num["septiembre"] = "09";
+mes2num["octubre"] = "10";
+mes2num["noviembre"] = "11";
+mes2num["diciembre"] = "12";
+
 titulo = document.title.replace(/ ?[\/\#\<\>\[\]\|\{\}\-].*/g,'');
 enlace = location.href;
 dominio = location.href.split('/')[2].split('.');dominio = dominio[dominio.length-2];
@@ -44,6 +57,7 @@ f['lavanguardia']='La Vanguardia';
 f['lavozdegalicia']='La Voz de Galicia';
 f['lavozdigital']='La Voz Digital';
 f['levante-emv']='Levante-EMV';
+f['librered']='LibreRed';
 f['madrilonia']='Madrilonia';
 f['mundoobrero']='Mundo Obrero';
 f['periodismohumano']='Periodismo Humano';
@@ -53,25 +67,33 @@ f['sevillareport']='Sevilla Report';
 f['tercerainformacion']='Tercera Información';
 fuente = f[dominio];
 if (!fuente) { fuente = dominio; }
- 
-fecha = '';
-fechaurl = enlace.match(/\/((20|19)[0-9][0-9][\/\-][0-1][0-9][\/\-][0-1][0-9])\//);
-if (fechaurl) {
-fecha = fechaurl[1].replace(/[\/\-]/g, '-');
-} else {
+
 body = document.body.innerHTML;
-t = body.match(/([0-3]?[0-9][\/\-\.][0-1]?[0-9][\/\-\.](20|19)[0-9][0-9])/);
-if (t) {
-fecha = t[0].replace(/[\/\.]/g,'-');
-fecha = pad(fecha.split('-')[2],4) + '-' + pad(fecha.split('-')[1],2) + '-' + pad(fecha.split('-')[0],2); 
-}else{
-t = body.match(/((20|19)[0-9][0-9][\/\-\.][0-1]?[0-9][\/\-\.][0-3]?[0-9])/);
-if (t) {
-fecha = t[0].replace(/[\/\.]/g,'-');
-fecha = pad(fecha.split('-')[0],4) + '-' + pad(fecha.split('-')[1],2) + '-' + pad(fecha.split('-')[2],2); 
+fecha = '';
+if (dominio == 'librered') {
+    t = body.match(/<li>[a-z]+, ([a-z]+) (\d+), (\d\d\d\d), \d+:\d+<\/li>/i);
+    mes = mes2num[t[1]];
+    dia = t[2];
+    if (dia.length == 1) { dia = '0'+dia; }
+    fecha = t[3] + '-' + mes + '-' + dia;
+} else {
+    fechaurl = enlace.match(/\/((20|19)[0-9][0-9][\/\-][0-1][0-9][\/\-][0-1][0-9])\//);
+    if (fechaurl) {
+        fecha = fechaurl[1].replace(/[\/\-]/g, '-');
+    } else {
+        t = body.match(/([0-3]?[0-9][\/\-\.][0-1]?[0-9][\/\-\.](20|19)[0-9][0-9])/);
+        if (t) {
+            fecha = t[0].replace(/[\/\.]/g,'-');
+            fecha = pad(fecha.split('-')[2],4) + '-' + pad(fecha.split('-')[1],2) + '-' + pad(fecha.split('-')[0],2); 
+        }else{
+            t = body.match(/((20|19)[0-9][0-9][\/\-\.][0-1]?[0-9][\/\-\.][0-3]?[0-9])/);
+            if (t) {
+                fecha = t[0].replace(/[\/\.]/g,'-');
+                fecha = pad(fecha.split('-')[0],4) + '-' + pad(fecha.split('-')[1],2) + '-' + pad(fecha.split('-')[2],2); 
+            }
+        }
+    }
 }
-}
-}
- 
+
 alert('Copia (Ctrl-C) el siguiente código y pégalo en la página de 15Mpedia apropiada:\n\n* {{noticia|titular='+titulo+'|enlace='+enlace+'|fuente='+fuente+'|fecha='+fecha+'}}\n\nNota: Repasa que los campos sean correctos');
 })();
