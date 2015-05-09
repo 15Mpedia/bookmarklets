9a0ae18614414167294591bf42a8e7ce7c85949f
Emilio J. Rodríguez-Posada <emijrp@gmail.com>
2015-05-09 13:52:07 +0200

    Update import-youtube-metadata.js

--- import-youtube-metadata.js
+++ import-youtube-metadata.js
@@ -1,32 +1,25 @@
 javascript:(function(){
-function pad(n, l) {
- var s = ''+parseInt(n);
- while (s.length<l) { s='0'+s; }   
- return s;
-}
- 
-url = document.querySelector('meta[property="og:url"]').content;
-videoid = url.split('v=')[1].split('&')[0];
-videouser = document.querySelectorAll('link[itemprop="url"]')[1].href.split('/')[4];
-title = document.querySelector('meta[property="og:title"]').content;
-imageurl = document.querySelector('meta[property="og:image"]').content;
- 
-desc = document.getElementById('eow-description').innerHTML;
-desc = desc.replace(/&quot;/g, '"');
-desc = desc.replace(/<br ?\/?>/g, '\n');
-desc = desc.replace(/<a href="([^>]*?)" [^>]*?>([^<]*?)<\/a>/g, '[$1 $2]');
- 
-date_ = document.body.innerHTML.match(/<strong>Publicado el ([^<]+?)<\/strong>/)[1];
-date_ = pad(date_.split('/')[2],4) + '-' + pad(date_.split('/')[1],2) + '-' + pad(date_.split('/')[0],2); 
-imagename = 'YouTube - '+videouser+' - '+videoid+'.jpg';
-license = '{{lye}}';
-if (document.getElementById('eow-reuse').innerHTML.match(/creative_commons/)) { license = '{{cc-by-3.0}}'; }
- 
-infobox = '{{Infobox Archivo\n|embebido=YouTube\n|embebido id='+videoid+'\n|embebido usuario='+videouser+'\n|embebido título='+title+'\n|descripción={{descripción de youtube|1='+desc+'}}\n|fecha de publicación='+date_+'\n|autor={{youtube channel|'+videouser+'}}\n|licencia='+license+'\n}}';
- 
-void(open('http://wiki.15m.cc/wiki/Special:UploadWindow'
-    + '?wpUploadFileURL=' + encodeURIComponent(imageurl)
-    + '&wpDestFile=' + encodeURIComponent(imagename)
-    + '&wpUploadDescription=' + encodeURIComponent(infobox),
-    'Import%20to%2015Mpedia'));
+    url = document.querySelector('meta[property="og:url"]').content;
+    videoid = url.split('v=')[1];
+    videouser = document.querySelectorAll('link[itemprop="url"]')[1].href.split('/')[4];
+    title = document.querySelector('meta[property="og:title"]').content;
+    imageurl = document.querySelector('meta[property="og:image"]').content;
+    
+    desc = document.getElementById('eow-description').innerHTML;
+    desc = desc.replace(/&quot;/g, '"');
+    desc = desc.replace(/<br ?\/?>/g, '\n');
+    desc = desc.replace(/<a href="([^>]*?)" [^>]*?>([^<]*?)<\/a>/g, '[$1 $2]');
+    
+    date_ = document.querySelector('meta[itemprop="datePublished"]').content;
+    imagename = 'YouTube - '+videouser+' - '+videoid+'.jpg';
+    license = '{{lye}}';
+    if (document.getElementById('eow-reuse') && document.getElementById('eow-reuse').innerHTML.match(/creative_commons/)) { license = '{{cc-by-3.0}}'; }
+    
+    infobox = '{{Infobox Archivo\n|embebido=YouTube\n|embebido id='+videoid+'\n|embebido usuario='+videouser+'\n|embebido título='+title+'\n|descripción={{descripción de youtube|1='+desc+'}}\n|fecha de publicación='+date_+'\n|autor={{youtube channel|'+videouser+'}}\n|licencia='+license+'\n}}';
+     
+    void(open('http://15mpedia.org/wiki/Special:UploadWindow'
+        + '?wpUploadFileURL=' + encodeURIComponent(imageurl)
+        + '&wpDestFile=' + encodeURIComponent(imagename)
+        + '&wpUploadDescription=' + encodeURIComponent(infobox),
+        'Import%20to%2015Mpedia%20'+videoid));
 })();
