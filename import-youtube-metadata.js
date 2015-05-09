b1d6802fa70c96a68128b6906cc9104601b671f5
Emilio J. Rodríguez-Posada <emijrp@gmail.com>
2015-05-09 14:12:39 +0200

    Update import-youtube-metadata.js

--- import-youtube-metadata.js
+++ import-youtube-metadata.js
@@ -2,7 +2,7 @@ javascript:(function(){
     url = document.querySelector('meta[property="og:url"]').content;
     videoid = url.split('v=')[1];
     videouser = document.querySelectorAll('link[itemprop="url"]')[1].href.split('/')[4];
-    title = document.querySelector('meta[property="og:title"]').content;
+    title = document.getElementById('eow-title').title;        
     imageurl = document.querySelector('meta[property="og:image"]').content;
     
     desc = document.getElementById('eow-description').innerHTML;
@@ -16,7 +16,7 @@ javascript:(function(){
     if (document.getElementById('eow-reuse') && document.getElementById('eow-reuse').innerHTML.match(/creative_commons/)) { license = '{{cc-by-3.0}}'; }
     
     infobox = '{{Infobox Archivo\n|embebido=YouTube\n|embebido id='+videoid+'\n|embebido usuario='+videouser+'\n|embebido título='+title+'\n|descripción={{descripción de youtube|1='+desc+'}}\n|fecha de publicación='+date_+'\n|autor={{youtube channel|'+videouser+'}}\n|licencia='+license+'\n}}';
-     
+    
     void(open('http://15mpedia.org/wiki/Special:UploadWindow'
         + '?wpUploadFileURL=' + encodeURIComponent(imageurl)
         + '&wpDestFile=' + encodeURIComponent(imagename)
