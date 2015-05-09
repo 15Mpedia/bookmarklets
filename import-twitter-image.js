2c895ce8a1a4b37a7f385328ee7de91e66603c0b
Emilio J. Rodríguez-Posada <emijrp@gmail.com>
2015-05-09 12:55:56 +0200

    Update import-twitter-image.js

--- import-twitter-image.js
+++ import-twitter-image.js
@@ -9,24 +9,31 @@ javascript:(function(){
         return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]) + ' ' + (hour[1]?hour:"0"+hour[0]) + ':' + (min[1]?min:"0"+min[0]) + ':' + (sec[1]?sec:"0"+sec[0]);
     };
     
-    description = document.querySelector('meta[property="og:description"]').content;
-    imageurl = document.querySelector('meta[property="og:image"]').content;
-    tweeturl = document.querySelector('meta[property="og:url"]').content;
-    tweetsplits = tweeturl.split('/');
-    username = tweetsplits[tweetsplits.length-3];
-    tweetid = tweetsplits[tweetsplits.length-1];
-    imagename = 'Twitter - '+username+' - '+tweetid+'.jpg';
-    
-    desc = '{{descripción de twitter|1='+description+'}}';
-    fuente = '{{tweet|'+username+'|'+tweetid+'}}';
-    fechapub = document.body.innerHTML.match(/data-time-ms="([0-9]+?)"/)[1];
-    fechapub = new Date(parseInt(fechapub)).yyyymmddhhmmss(); 
-    
-    infobox = '{{Infobox Archivo\n| descripción = '+desc+'\n| fuente = '+fuente+'\n| fecha de publicación = '+fechapub+'\n| autor = {{twitter|'+username+'}}\n}}';
-     
-    void(open('http://15mpedia.org/wiki/Special:UploadWindow'
-        + '?wpUploadFileURL=' + encodeURIComponent(imageurl)
-        + '&wpDestFile=' + encodeURIComponent(imagename)
-        + '&wpUploadDescription=' + encodeURIComponent(infobox),
-        'Import%20to%2015Mpedia'));
+    images = document.querySelectorAll('meta[property="og:image"]');
+    for (i=0;i<images.length;i++) {
+        description = document.querySelector('meta[property="og:description"]').content;
+        imageurl = images[i].content;
+        tweeturl = document.querySelector('meta[property="og:url"]').content;
+        tweetsplits = tweeturl.split('/');
+        username = tweetsplits[tweetsplits.length-3];
+        tweetid = tweetsplits[tweetsplits.length-1];
+        imageid = imageurl.split('/');
+        imageid = imageid[imageid.length-1];
+        imageid = imageid.split('.');
+        imageid = imageid[0];
+        imagename = 'Twitter - '+username+' - '+tweetid+' - '+imageid+'.jpg';
+        
+        desc = '{{descripción de twitter|1='+description+'}}';
+        fuente = '{{tweet|'+username+'|'+tweetid+'}}';
+        fechapub = document.body.innerHTML.match(/data-time-ms="([0-9]+?)"/)[1];
+        fechapub = new Date(parseInt(fechapub)).yyyymmddhhmmss(); 
+        
+        infobox = '{{Infobox Archivo\n| descripción = '+desc+'\n| fuente = '+fuente+'\n| fecha de publicación = '+fechapub+'\n| autor = {{twitter|'+username+'}}\n}}';
+         
+        void(open('http://15mpedia.org/wiki/Special:UploadWindow'
+            + '?wpUploadFileURL=' + encodeURIComponent(imageurl)
+            + '&wpDestFile=' + encodeURIComponent(imagename)
+            + '&wpUploadDescription=' + encodeURIComponent(infobox),
+            'Import%20to%2015Mpedia%20'+imagename));
+    }
 })();
